573dc456b38e60b780aa5d419231d3571aada103
ALockett89 <aklockett@fullsail.edu>
2015-06-12 00:14:59 -0500

    Slice of Pie part II
    Added Slice of Pie part II

---
+++ Expressions_worksheet/Lockett_Anthony_Expressions_Assignment/Slice of Pie part II/js/script.js
@@ -0,0 +1,22 @@
+alert("I hope I got this right");
+
+//Slice of Pie part II
+
+//How many whole slices will Sparky feast on?
+var slice = 8// Number of slices per pizza.
+var people = 40// Number of people at the party.
+var pizzas = 15// Number of pizzas ordered.
+var spp = pizzas * slice % people// This is the result.
+var sparkyscut = spp
+var a = "Sparky"// This is the variable for the word "Each".
+var b = "got"// This is the variable for the word "person".
+var c = "ate"// This is the variable for the word "ate".
+var d = "slices"// This is the variable for the word "slices".
+var e = "of"// This is the variable for the word "of".
+var f = "pizza"// This is the variable for the word "pizza".
+var g = "at"// This is the variable for the word "at".
+var h = "the"// This is the variable for the word "the".
+var i = "party"// This is the variable for the word "party".
+var j = "."// This is the variable for the punctuation ".".
+var answer = a + " " + b + " " + sparkyscut + " " + d + " " + e + " " + f + j// This is what shows in the console.
+console.log(answer)// This is what shows in the console.
