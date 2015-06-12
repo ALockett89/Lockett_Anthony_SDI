bdc65f475defd83a3a05ee27a394d346b85f293c
ALockett89 <aklockett@fullsail.edu>
2015-06-11 22:13:59 -0500

    Dog Years
    Done with Dog Years problem code.

---
+++ Dog Years/js/script.js
@@ -0,0 +1,18 @@
+// Dog Years
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+var dogyears = 7// This is how many dog years that are in one human year.
+var sparky = 5// This is how old sparky is.
+var humanyears = 1// This is the number of human years.
+var sparkysage = sparky * humanyears * dogyears// This is the result variable.
+var a = "Sparky"// This is the variable for the word "Sparky".
+var b = "is"// This is the variable for the word "is".
+var c = "human"// This is the variable for the word "human".
+var d = "years"// This is the variable for the word "years".
+var e = "old"// This is the variable for the word "old".
+var f = "which"// This is the variable for the word "which".
+var h = "in"// This is the variable for the word "in".
+var i = "dog"// This is the variable for the word "dog".
+var j = "."// This is the variable for the punctuation ".".
+var answer = a + " " + b + " " + sparky + " " + c + " " + d + " " + e + " " + f + " " + b + " " + sparkysage + " " + h + " " + i +  " " + d + j// This is what shows in the console.
+console.log(answer);
