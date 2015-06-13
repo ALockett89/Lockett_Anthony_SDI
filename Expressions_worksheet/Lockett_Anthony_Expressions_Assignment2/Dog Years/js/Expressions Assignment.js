14c340c29e6d05c62ab3452ebbca5d1505fa2418
ALockett89 <aklockett@fullsail.edu>
2015-06-13 13:57:34 -0500

    Added the meat
    I added the prompts and alerts to get and display the user input
    information.

--- Expressions_worksheet/Lockett_Anthony_Expressions_Assignment2/Dog Years/js/Expressions Assignment.js
+++ Expressions_worksheet/Lockett_Anthony_Expressions_Assignment2/Dog Years/js/Expressions Assignment.js
@@ -2,5 +2,10 @@
 //June 11, 2015
 // Expressions Assignment
 
-//This should help a user calculate how many hours they have worded in a week.
-
+//This should help a user calculate how many hours they have worked in a pay period.
+var daysworked = prompt("So how many days did you work this week?")
+var hoursinday = prompt("How many hours do you work a day")
+var weekstilpay = prompt("How many weeks in between pay checks? \n1 or 2?")
+var hoursinweek = daysworked * hoursinday
+var check = hoursinweek * weekstilpay
+alert("Congratulations!!! \nYou will have" + " " + check + "hours on you next pay check!")
