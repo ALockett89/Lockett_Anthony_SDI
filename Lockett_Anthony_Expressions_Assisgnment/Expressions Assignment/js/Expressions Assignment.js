d47f2401a3fc07f35a9e2696e7b70467076dffac
ALockett89 <aklockett@fullsail.edu>
2015-06-13 14:50:23 -0500

    Comments v2
    Added comments to the console logs to explain them and fixed a grammar
    error in one of the console logs.

--- Lockett_Anthony_Expressions_Assisgnment/Expressions Assignment/js/Expressions Assignment.js
+++ Lockett_Anthony_Expressions_Assisgnment/Expressions Assignment/js/Expressions Assignment.js
@@ -8,9 +8,9 @@ var hoursinday = prompt("How many hours do you work a day?")// This prompt asks 
 var weekstilpay = prompt("How many weeks in between pay checks? \n1 or 2?")// This prompt asks the user for how many week(s) are between their paychecks.
 var hoursinweek = daysworked * hoursinday// This variable calculates how many hours were worked in 1 week.
 var check = hoursinweek * weekstilpay// This variable calculates the hours on the user's next paycheck.
-alert("Congratulations!!! \nYou will have" + " " + check + "hours on you next pay check!")
-console.log("Days worked:" + " " + daysworked)
-console.log("Hours worked each day:" + " " + hoursinday)
-console.log("Weeks between pay checks:" + " " + weekstilpay)
-console.log("Hours worked in a week:" + " " + hoursinweek)
-console.log("The answer the user gets:" + " " + check)
+alert("Congratulations!!! \nYou will have" + " " + check + "hours on you next pay check!")// This alert shows the user how many hours that will be on their pay check based on their inputs.
+console.log("Days worked:" + " " + daysworked)// Shows the days worked in the console.
+console.log("Hours worked each day:" + " " + hoursinday)// Shows the hours worked each day in the console.
+console.log("Weeks between paychecks:" + " " + weekstilpay)// Shows the weeks between each paychecks in the console.
+console.log("Hours worked in a week:" + " " + hoursinweek)// Shows the hours worked in a week in the console.
+console.log("The answer the user gets:" + " " + check)// Shows the the answer the user gets in the console.
