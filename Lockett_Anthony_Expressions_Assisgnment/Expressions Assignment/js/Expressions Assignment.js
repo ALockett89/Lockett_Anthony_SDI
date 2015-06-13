b1239e5539abf7f7f98ac078e8d82025d35e53d9
ALockett89 <aklockett@fullsail.edu>
2015-06-13 14:26:40 -0500

    Grammar Fixes
    Found a few grammar errors and fixed them.

--- Lockett_Anthony_Expressions_Assisgnment/Expressions Assignment/js/Expressions Assignment.js
+++ Lockett_Anthony_Expressions_Assisgnment/Expressions Assignment/js/Expressions Assignment.js
@@ -4,7 +4,7 @@
 
 //This should help a user calculate how many hours they have worked in a pay period.
 var daysworked = prompt("So how many days did you work this week?")
-var hoursinday = prompt("How many hours do you work a day")
+var hoursinday = prompt("How many hours do you work a day?")
 var weekstilpay = prompt("How many weeks in between pay checks? \n1 or 2?")
 var hoursinweek = daysworked * hoursinday
 var check = hoursinweek * weekstilpay
