0ab2cf139dd6e47d0a8b9851884a9522a40eb521
ALockett89 <aklockett@fullsail.edu>
2015-06-16 01:13:24 -0500

    First part done
    I finished the first part of my first “IF” in my code.

---
+++ Lockett_Anthony_Conditionals _Assignment/Conditionals Assignment/js/ConditionalsAssignment.js
@@ -0,0 +1,28 @@
+/*
+ Anthony Lockett
+ June 13, 2015
+ Group 1: Expressions with Conditionals
+ */
+
+//Celsius to Fahrenheit converter
+
+var paytype = prompt("How do you get paid? \n 1. Weekly \n 2. Bi-Weekly")
+if(paytype <= 1){
+ var hoursaday = prompt("How many hours do you work a day?")
+ var hourwage = prompt("How much is your hourly wage?")
+ var daysworked = prompt("How many days do you work in a week?")
+ var weekhours1 = hoursaday * daysworked
+ var gross = hourwage * weekhours1
+ var net = prompt("Do you pay taxes? \n 1. Yes \n 2. No")
+ if(net >1){
+  alert("You wage is" + " " + gross)
+ }else{var net2 = gross * .9
+  alert("Your wage is" + " " + net2)}
+}else{alert("Bi-Weekly")
+}
+console.log("Hours A Day:" + hoursaday)
+console.log("Hourly Wage:" + hourwage)
+console.log("Days Worked A Week:" + daysworked)
+console.log("Hours Worked A Week:" + weekhours1)
+console.log("Gross Wage:" + gross)
+console.log("Net Wage:" + net2)
