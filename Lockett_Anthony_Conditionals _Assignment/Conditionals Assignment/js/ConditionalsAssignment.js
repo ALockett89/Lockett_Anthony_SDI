41cf6b55ccb6dd88a85122bc20f45f30e06f2317
ALockett89 <aklockett@fullsail.edu>
2015-06-18 18:57:27 -0500

    1st round of comments
    Added the first round of comments.

--- Lockett_Anthony_Conditionals _Assignment/Conditionals Assignment/js/ConditionalsAssignment.js
+++ Lockett_Anthony_Conditionals _Assignment/Conditionals Assignment/js/ConditionalsAssignment.js
@@ -6,19 +6,19 @@
 
 //Calculate your wages & see if you are BALLING!
 
-var paytype = prompt("How do you get paid? \n 1. Weekly \n 2. Bi-Weekly \n **Enter Number(s) Only**")
-if(paytype <= 1){
- var hoursaday = prompt("How many hours do you work a day? \n **Enter Number(s) Only**")
- var hourwage = prompt("How much is your hourly wage? \n **Enter Number(s) Only**")
- var daysworked = prompt("How many days do you work in a week? \n **Enter Number(s) Only**")
- var weekhours1 = hoursaday * daysworked
- var gross = hourwage * weekhours1
- var net = prompt("Do you pay taxes? \n 1. Yes \n 2. No \n **Enter Number(s) Only**")
- if(net >1){
-  alert("You wage is" + " " + gross)
- }else{var net2 = gross * .9
-  alert("Your wage is" + " " + net2)}
-}else{alert("Bi-Weekly? Ok!")
+var paytype = prompt("How do you get paid? \n 1. Weekly \n 2. Bi-Weekly \n **Enter Number(s) Only**")// This variable for the users pay type.
+if(paytype <= 1){ // This condition directs which section of code to run base on the user's input.
+ var hoursaday = prompt("How many hours do you work a day? \n **Enter Number(s) Only**")// This variable asks for the user's hours worked in a day.
+ var hourwage = prompt("How much is your hourly wage? \n **Enter Number(s) Only**")// This variable asks for how much is the user's hourly pay.
+ var daysworked = prompt("How many days do you work in a week? \n **Enter Number(s) Only**")// This variable asks for how many days the user works a week.
+ var weekhours1 = hoursaday * daysworked// This variable calculates the hours worked in a week.
+ var gross = hourwage * weekhours1// This variable calculates the user's gross income.
+ var net = prompt("Do you pay taxes? \n 1. Yes \n 2. No \n **Enter Number(s) Only**")// This variable calculates the user's net pay based on Louisiana's 9% tax rate.
+ if(net >1){ // This condition directs which section of code to run base on the user's input.
+  alert("You wage is" + " " + gross)// This alert notifies the user of their gross amount.
+ }else{var net2 = gross * .9// This condition directs which section of code to run base on the user's input.
+  alert("Your wage is" + " " + net2)}// This alert notifies the user of their net amount.
+}else{alert("Bi-Weekly? Ok!") // This condition directs which section of code to run base on the user's input.
  var hoursaday = prompt("How many hours do you work a day \n **Enter Number(s) Only**?")
  var hourwage = prompt("How much is your hourly wage? \n **Enter Number(s) Only**")
  var daysworked = prompt("How many days do you work in a week? \n **Enter Number(s) Only**")
