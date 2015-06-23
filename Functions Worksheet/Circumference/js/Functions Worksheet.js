cd5986db4d7246ee5d089c204d102bf447e827b8
ALockett89 <aklockett@fullsail.edu>
2015-06-23 00:00:47 -0500

    First function done

---
+++ Functions Worksheet/Circumference/js/Functions Worksheet.js
@@ -0,0 +1,26 @@
+/*
+ Anthony Lockett
+ SDI Week 4
+ Functions Assignment
+ 6/21/15
+ */
+
+//Circumference
+
+//var Diameter = prompt("What is your Diameter? \n *Use Numbers Only*")
+//var Radius = Diameter / 2
+//alert(Radius)
+
+function diameter(){
+    var diameter = 186
+    var radius = diameter / 2
+    console.log(radius)
+
+}
+diameter()
+
+
+
+
+
+console.log("The circumference of the circle is....")
