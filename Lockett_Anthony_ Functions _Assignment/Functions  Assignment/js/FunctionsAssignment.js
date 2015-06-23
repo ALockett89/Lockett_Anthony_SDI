57337b522d20345d1b0f1e688780ce7b09d418b6
ALockett89 <aklockett@fullsail.edu>
2015-06-23 01:34:46 -0500

    Added my first function
    Used a while loop to validate if left blank or not a number

--- Lockett_Anthony_ Functions _Assignment/Functions  Assignment/js/FunctionsAssignment.js
+++ Lockett_Anthony_ Functions _Assignment/Functions  Assignment/js/FunctionsAssignment.js
@@ -4,4 +4,21 @@ SDI Week 4
 Functions Assignment
 6/21/15
  */
-alert("This is in the js file");
+
+ //Calculation Of Age
+
+dateoftest()
+
+function dateoftest(){
+    var testyear = prompt("What is today's year?")
+    while(testyear==="" || isNaN(testyear)){
+        if(testyear===""){
+            testyear = prompt("PLEASE DO NOT LEAVE BLANK!!!\n What is today's year?")
+        }else if(isNaN(testyear)){
+            testyear = prompt("ONLY USE NUMBERS!!!\n What is today's year?")
+
+        }
+
+
+    }
+}
